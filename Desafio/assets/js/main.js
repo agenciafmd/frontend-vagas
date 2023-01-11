5f3dee81eb24b733a74484da7908391b7b804c01
lilian-rangel <lilianrm.nutri@gmail.com>
2023-01-11 17:18:03 -0300

    :construction: feat(seção cards 2): crie seção avaliações com 3 cards

--- Desafio/assets/js/main.js
+++ Desafio/assets/js/main.js
@@ -6,36 +6,22 @@ window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
 });
 
-/* swiper
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
- 
-  }); */
-
-  var swiper = new Swiper(".home-swiper", {
+// Slider do home
+var swiper = new Swiper(".home-swiper", {
     spaceBetween: 20,
     loop: true,
     pagination: {
         el: '.swiper-pagination',
-      },
-    
-      navigation: {
+    },
+
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
+    },
     autoplay: {
-      delay: 5500,
-      disableOnInteraction: false,
+        delay: 5500,
+        disableOnInteraction: false,
     },
     centeredSlides: true,
-  });
+});
+
