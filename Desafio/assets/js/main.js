9cb764e4bed6d54ec7d3ed7950ab8b711ba91c0d
lilian-rangel <lilianrm.nutri@gmail.com>
2023-01-11 20:06:41 -0300

    :construction: feat(seção newsletter/footer): cria seção de newsletter| cria modal ao clicar em se inscrever newsletter| cria seção footer

--- Desafio/assets/js/main.js
+++ Desafio/assets/js/main.js
@@ -25,3 +25,14 @@ var swiper = new Swiper(".home-swiper", {
     centeredSlides: true,
 });
 
+// Modal 
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function () {
+    modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function () {
+    modal.classList.remove("open-modal");
+});
