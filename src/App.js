5db81d8a46d5eb10b904040c6e886be47fa71345
Adriane maciel <adrianemaciel.developer@gmail.com>
2023-01-12 18:43:19 -0300

    project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import Home from "./page/Home";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
