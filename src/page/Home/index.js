561819208df4c95895596bb40cffb0bb58d92d9d
Adriane maciel <adrianemaciel.developer@gmail.com>
2023-01-13 09:17:51 -0300

    axios get pokemons list

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,9 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import Banner from "../../components/Banner";
 import Header from "../../components/Header";
 import { Container } from "./styles";
 
 function Home() {
+  async function getPokemon() {
+    await axios
+      .get("https://pokeapi.co/api/v2/pokemon/", {
+        params: {
+          offset: 0,
+          limit: 4,
+        },
+      })
+      .then(function (response) {
+        console.log("response", response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
   return (
     <Container>
       <Header />
