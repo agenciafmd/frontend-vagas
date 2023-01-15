ab97688a753d7251a207dc43a124dfd53ce6735b
cleitonSam <cleiton.nso2019@gmail.com>
2023-01-15 11:42:52 -0300

    style(footer): criando footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
