d6913039261756dc97ea8ae6c9616edab82d6fd1
Nathan Lucas <n54318879@gmail.com>
2023-01-17 00:30:38 -0300

    feat em src/main: prepare o ambiante para o Chakra

--- Landing_page/src/main.jsx
+++ Landing_page/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './lib/theme'
+import './Global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
