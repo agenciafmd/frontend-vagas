c6f00ec09bbf5f781be13f82b8ffb53a8e655753
Nathan Lucas <n54318879@gmail.com>
2023-01-17 01:05:07 -0300

    feat App: coloque as funcionalidades dentro de App

--- Landing_page/src/App.jsx
+++ Landing_page/src/App.jsx
@@ -1,7 +1,34 @@
+import { Header } from './Components/Header/Header'
+import { Home } from './Pages/Section1'
+import { Section_TopRated } from './Pages/Section2'
+import { useEffect, useState, useRef } from 'react'
+import { SlideCards } from './Pages/Section3';
+import { FormNewsLetter } from './Pages/Section4';
+import { Footer } from './Components/Footer/Footer';
+
+const Api_Url = 'https://api.themoviedb.org/3/movie/';
+const Api_Key = 'api_key=5388c3a2adab3279b15814d613079c99';
+
 function App() {
+  const [info, setInfo ] = useState([]);
+
+  useEffect(() => {
+
+    fetch(`${Api_Url}top_rated?${Api_Key}`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      setInfo(data.results)
+    })
+  }, [])
 
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <Home/>
+      <Section_TopRated movie={info}/>
+      <SlideCards movie={info}/>
+      <FormNewsLetter/>
+      <Footer/> 
     </div>
   )
 }
