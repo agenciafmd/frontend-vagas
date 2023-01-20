488ca0689ccfae31bbc55b40c50ee9069f8e0a61
Matheus Costa <matheuscostadosantos@hotmail.com>
2023-01-20 19:04:01 -0300

    refact(app): altera chamada biblioteca aos

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./styles/global.scss";
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
